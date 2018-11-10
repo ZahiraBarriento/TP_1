6aa7dfe5f4717fd047ea0e9dc99d3d7181323f74
Zahira Barriento <40859080+ZahiraBarriento@users.noreply.github.com>
2018-11-09 22:32:24 -0300

    Update parser.c

--- TP_3/parser.c
+++ TP_3/parser.c
@@ -10,54 +10,60 @@
 ///ABRIR EN FORMA DE TEXTO
 int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee)
 {
-    Employee* auxEmployee = employee_new();
-    int r, i;
-    char auxId[50], auxNombre[50], auxHorasTrabajadas[50], auxSueldo[50];
-
-    r = fscanf(pFile, "%[^,],%[^,],%[^,],%s\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
-
-    while(!feof(pFile))
+    if(pArrayListEmployee !=NULL)
     {
+        Employee* auxEmployee = employee_new();
+        int r, i;
+        char auxId[50], auxNombre[50], auxHorasTrabajadas[50], auxSueldo[50];
+
         r = fscanf(pFile, "%[^,],%[^,],%[^,],%s\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
 
-        if(r == 4)
+        while(!feof(pFile))
         {
-            auxEmployee = employee_newParametros(auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
-            ll_add(pArrayListEmployee,auxEmployee);
-            i++;
-        }
-        else
-        {
-            printf("\n ERROR. No se ha leido correctamente.\n\n");
-        }
-    }
+            r = fscanf(pFile, "%[^,],%[^,],%[^,],%s\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
 
-    printf("\n Empleados cargados correctamente (modo texto).\n\n");
-    fclose(pFile);
+            if(r == 4)
+            {
+                auxEmployee = employee_newParametros(auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
+                ll_add(pArrayListEmployee,auxEmployee);
+                i++;
+            }
+            else
+            {
+                printf("\n ERROR. No se ha leido correctamente.\n\n");
+            }
+        }
+
+        printf("\n Empleados cargados correctamente (modo texto).\n\n");
+        fclose(pFile);
+    }
     return 1;
 }
 
 int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee)
 {
-    Employee employee;
-    int i = 0;
-
-    fread(&employee, sizeof(Employee), 1, pFile);
-
-    while(!feof(pFile))
+    if(pArrayListEmployee !=NULL)
     {
-        Employee* this = employee_new();
-        employee_setId(this,employee.id);
-        employee_setHorasTrabajadas(this,employee.horasTrabajadas);
-        employee_setNombre(this,employee.nombre);
-        employee_setSueldo(this,employee.sueldo);
+        Employee employee;
+        int i = 0;
 
-        i ++;
-        ll_add(pArrayListEmployee, this);
         fread(&employee, sizeof(Employee), 1, pFile);
-    }
 
-    printf("\n Empleados cargados correctamente (modo binario).\n\n");
+        while(!feof(pFile))
+        {
+            Employee* this = employee_new();
+            employee_setId(this,employee.id);
+            employee_setHorasTrabajadas(this,employee.horasTrabajadas);
+            employee_setNombre(this,employee.nombre);
+            employee_setSueldo(this,employee.sueldo);
+
+            i ++;
+            ll_add(pArrayListEmployee, this);
+            fread(&employee, sizeof(Employee), 1, pFile);
+        }
+
+        printf("\n Empleados cargados correctamente (modo binario).\n\n");
+    }
     return 1;
 }
 
